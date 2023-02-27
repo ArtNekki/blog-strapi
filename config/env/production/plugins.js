6d719b75fea4873119d28ac0786e427ed8287a78
a.mayatskiy <work.mayatskiy@gmail.com>
2023-02-27 18:28:55 +1000

    feat: add cloudinary

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -14,10 +14,25 @@ module.exports = ({ env }) => ({
                 // ... any custom nodemailer options
             },
             settings: {
-                defaultFrom: 'info@artnekki.com',
-                defaultReplyTo: 'info@artnekki.com',
+                defaultFrom: 'work.mayatskiy@gmail.com',
+                defaultReplyTo: 'work.mayatskiy@gmail.com',
             },
         },
     },
-    // ...
+
+    upload: {
+        config: {
+            provider: 'cloudinary',
+            providerOptions: {
+                cloud_name: env('CLOUDINARY_NAME'),
+                api_key: env('CLOUDINARY_KEY'),
+                api_secret: env('CLOUDINARY_SECRET'),
+            },
+            actionOptions: {
+                upload: {},
+                uploadStream: {},
+                delete: {},
+            },
+        },
+    },
 });
